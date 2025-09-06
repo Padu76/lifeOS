17ef5f26a159fd14cbe10fb46f4939b484ce719e
Padu76 <andrea.padoan@gmail.com>
2025-09-06 16:46:16 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,69 @@
 module.exports = {
   root: true,
-  extends: [
-    '@typescript-eslint/recommended',
-    'next/core-web-vitals',
-    'prettier'
-  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2022,
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
+    project: './tsconfig.json',
   },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'react-native',
+    'import',
+    'jsx-a11y',
+  ],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    '@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-native/all',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+  ],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'error',
+    // Existing rules
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-explicit-any': 'warn',
     'prefer-const': 'error',
-    'no-var': 'error'
+    'no-var': 'error',
+    
+    // Additional LifeOS specific rules
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    '@typescript-eslint/prefer-optional-chain': 'error',
+    'react/jsx-key': 'error',
+    'react/no-array-index-key': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    
+    // Storybook files exceptions
+    'import/no-anonymous-default-export': 'off',
   },
-  ignorePatterns: ['dist/', 'node_modules/', '*.js', '.next/', 'out/']
+  ignorePatterns: [
+    'dist/', 
+    'node_modules/', 
+    '*.js', 
+    '.next/', 
+    'out/',
+    'storybook-static/',
+    '**/*.stories.tsx',
+    '**/*.test.tsx',
+    '**/*.spec.tsx'
+  ],
+  overrides: [
+    {
+      files: ['**/*.stories.tsx', '**/*.stories.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        'react-hooks/rules-of-hooks': 'off',
+      }
+    }
+  ]
 };
