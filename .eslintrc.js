652a1dcb5e61c01ebc703cd54870c0e523ca0964
Padu76 <andrea.padoan@gmail.com>
2025-09-06 18:17:41 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,78 +2,86 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2022,
+    ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
-    project: './tsconfig.json',
   },
-  plugins: [
-    '@typescript-eslint',
-    'react',
-    'react-hooks',
-    'react-native',
-    'import',
-    'jsx-a11y',
-  ],
   extends: [
-    'eslint:recommended',
     '@typescript-eslint/recommended',
-    '@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react-native/all',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'plugin:jsx-a11y/recommended',
+    'next/core-web-vitals',
+    'prettier'
   ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // TypeScript rules
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    '@typescript-eslint/prefer-optional-chain': 'error',
+    
+    // General rules
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-console': 'warn',
+    
+    // React rules
+    'react/jsx-key': 'error',
+    'react/no-array-index-key': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    
+    // Next.js specific
+    '@next/next/no-img-element': 'error',
+    '@next/next/no-html-link-for-pages': 'error',
+    
+    // Storybook files exceptions
+    'import/no-anonymous-default-export': 'off',
+  },
   settings: {
     react: {
       version: 'detect',
-    },
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: './tsconfig.json',
-      },
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
     },
   },
   env: {
     browser: true,
     node: true,
-    es2022: true,
-    'react-native/react-native': true,
+    es2020: true,
   },
-  rules: {
-    // TypeScript rules
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unsafe-assignment': 'warn',
-    '@typescript-eslint/no-unsafe-member-access': 'warn',
-    '@typescript-eslint/no-unsafe-call': 'warn',
-    '@typescript-eslint/no-unsafe-return': 'warn',
-    '@typescript-eslint/restrict-template-expressions': 'warn',
-    '@typescript-eslint/prefer-nullish-coalescing': 'error',
-    '@typescript-eslint/prefer-optional-chain': 'error',
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/await-thenable': 'error',
-    '@typescript-eslint/no-misused-promises': 'error',
-
-    // React rules
-    'react/react-in-jsx-scope': 'off', // Not needed in React 17+
-    'react/prop-types': 'off', // Using TypeScript for prop validation
-    'react/jsx-uses-react': 'off',
-    'react/jsx-uses-vars': 'error',
-    'react/no-unescaped-entities': 'warn',
-    'react/display-name': 'warn',
-    'react/jsx-key': 'error',
-    'react/jsx-no-duplicate-props': 'error',
-    'react/jsx-no-undef': 'error',
-    'react/jsx-pascal-case': 'error',
-    'react/no-array-index-key': 'warn',
-    'react/no-danger': 'warn',
-    'react/no-deprecate
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    '.next/',
+    'out/',
+    'storybook-static/',
+    '**/*.stories.tsx',
+    '**/*.test.tsx',
+    '**/*.spec.tsx',
+    'coverage/',
+    'build/',
+  ],
+  overrides: [
+    {
+      files: ['**/*.stories.tsx', '**/*.stories.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        'react-hooks/rules-of-hooks': 'off',
+        'no-console': 'off',
+      },
+    },
+    {
+      files: ['**/*.test.tsx', '**/*.test.ts', '**/*.spec.tsx', '**/*.spec.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-console': 'off',
+      },
+    },
+    {
+      files: ['apps/mobile/**/*'],
+      rules: {
+        '@next/next/no-img-element': 'off',
+        '@next/next/no-html-link-for-pages': 'off',
+      },
+    },
+  ],
+};
